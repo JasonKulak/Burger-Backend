e8d6c0e1b310f985178a1647f1b119dad568841b
Jason Kulak <JasonKulak@yahoo.com>
2024-04-03 18:02:09 -0500

    models update

--- burgers/models.py
+++ burgers/models.py
@@ -3,5 +3,11 @@ from django.db import models
 # Create your models here.
 class Burger(models.Model):
     nameOfBurger=models.CharField(max_length=200)
-    ingredients=models.CharField(max_length=200)
+    ingredientOne=models.CharField(max_length=200)
+    ingredientTwo=models.CharField(max_length=200)
+    ingredientThree=models.CharField(max_length=200)
+    ingredientFour=models.CharField(max_length=200)
+    ingredientFive=models.CharField(max_length=200)
+    ingredientSix=models.CharField(max_length=200)
+    ingredientSeven=models.CharField(max_length=200)
     haveEaten=models.BooleanField(default=False)
